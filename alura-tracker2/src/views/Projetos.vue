9c9cc186705329a966b271c361d0bcf9e0259499
Brener Araujo <brener.araujo@engegraph.com.br>
2023-12-20 10:18:54 -0300

    Alura Tracker 2

---
+++ alura-tracker2/src/views/Projetos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="projetos">
+        <hi class="title">Projetos</hi>
+        <RouterView></RouterView>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Projetos'
+})
+</script>
+
+<style scoped>
+.projetos {
+    padding: 1.25rem;
+}
+</style>
